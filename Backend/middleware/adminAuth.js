971c82cfd3c62496a9582d4cc6bf4e51e7203b2f
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2023-09-05 16:22:07 +0100

    just little

--- Backend/middleware/adminAuth.js
+++ Backend/middleware/adminAuth.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
 
-const forAdmin = async (req, res, next) => {
-    const cookie = req.headers
-    console.log(cookie)
-    if(cookie){
-        const isAdmin = cookie.split('=')[0]
-        const token = cookie.split('=')[1]
+const theAdmin = async (req, res, next) => {
+    const {cookie} = req.headers
+    const splitter = cookie.split(' ')
+    splitter.forEach(element => {
+        const isAdmin = element.split('=')[0]
+        const token = element.split('=')[1]
 
         if(isAdmin == 'isAdmin'){
             // ======= Verifying token ========= //
@@ -21,9 +21,56 @@ const forAdmin = async (req, res, next) => {
         }else{
             res.status(401).json({errors: 'sorry, you are not allowed to perform this Operation'})
         }
-    }else{
-        res.status(401).json({errors: 'sorry, you are not allowed to perform this Operation'})
+    });
+    // if(cookie){
+    //     const isAdmin = cookie.split('=')[0]
+    //     const token = cookie.split('=')[1]
 
+    //     if(isAdmin == 'isAdmin'){
+    //         // ======= Verifying token ========= //
+    //         jwt.verify(token, process.env.JWT_TOKEN, (err, decodedToken)=>{
+    //             if(err){
+    //                 res.status(401).json({errors: 'sorry, you are not allowed to perform this Operation'})
+    //             }
+    //             else{
+    //                 req.user = decodedToken
+    //                 next()
+    //             }
+    //         })
+    //     }else{
+    //         res.status(401).json({errors: 'sorry, you are not allowed to perform this Operation'})
+    //     }
+    // }else{
+    //     res.status(401).json({errors: 'sorry, you are not allowed to perform this Operation'})
+
+    // }
+}
+
+const forAdmin = async (req, res, next) => {
+    const isAdmin = req.headers.cookie
+    if(isAdmin){
+        const splitter = isAdmin.split(' ')
+        splitter.map(cookie => {
+            const cookieName = cookie.split('=')[0]
+            const cookieValue = cookie.split('=')[1]
+            if(cookieName == 'isAdmin'){
+                return (
+                    jwt.verify(cookieValue, process.env.JWT_TOKEN, (err, decodedToken)=>{
+                        if(err){
+                            res.status(401).json({errors: 'You are not allowed to perform this operation'})
+                        }else{
+                            req.user = decodedToken
+                            next()
+                        }
+                    })
+                )
+            }
+            else{
+                return( res.status(401).json({errors: 'You are not allowed to perform this operation'}))
+            }
+        })
+    }else{
+        res.status(401).json({errors: 'Not Allowed'})
     }
 }
 
--- Backend/middleware/adminUserChecker.js
+++ Backend/middleware/adminUserChecker.js
@@ -3,13 +3,17 @@ const Mngmodel = require('../models/schoolMng')
 
 
 const currentAdmin = async (req, res, next) => {
-    const {id} = req.user
-    const {role, name, email} = await Mngmodel.findById(id)
-    if(!role == 'admin'){
-        return (res.status(401).json({ errors: 'sorry, you are not allowed to perform this Operation' }))
+    try {
+        const {id} = req.user
+        const {role, name, email} = await Mngmodel.findById(id)
+        if(!role === 'admin'){
+            return (res.status(401).json({ errors: 'sorry, you are not allowed to perform this Operation' }))
+        }
+        req.user = {role, name, email}
+        next()
+    } catch (error) {
+        res.status(401).json({errors: 'something went wrong, please try again later'})
     }
-    req.user = {role, name, email}
-    next()
 }
 
 
